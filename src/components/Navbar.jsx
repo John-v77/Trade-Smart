5285ea7e8978bcde849d81856680514f31a3a362
John-v77 <ionut_vasile85@hotmail.com>
2021-05-21 16:45:35 -0400

    Navbar logo fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-// import logo from '../images/logo.jpg';
+import logo from '../images/logo.jpg';
 
 function Navbar(props) {
     return (
         <div className="navBar shadow">
-            {/* <img src='/' alt='logo'/> */}
+            <img src={logo} alt='logo'/>
             <Link to='/'>Home</Link>
             <Link to='/Porfolio'>Portfolio</Link>
             <Link to='/WatchList'>Watch List</Link>
