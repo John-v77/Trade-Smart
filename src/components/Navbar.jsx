b56881bd619f8353a13e2186892cd75708951a71
John-v77 <ionut_vasile85@hotmail.com>
2021-05-21 16:15:29 -0400

    navBar style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+// import logo from '../images/logo.jpg';
 
 function Navbar(props) {
     return (
-        <div className="navBar">
-            
+        <div className="navBar shadow">
+            <img src='/' alt='logo'/>
+            <Link to='/'>Home</Link>
+            <Link to='/'>Portfolion</Link>
+            <Link to='/'>Home</Link>
+            <Link to='/'>Contact</Link>           
         </div>
     );
 }
