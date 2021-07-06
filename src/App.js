8542dad0d3a62b270751443e991e9da5c151de22
john-v77 <tinyZ@tutanota.com>
2021-07-06 18:47:53 -0400

    banner for desktop only experience

--- src/App.js
+++ src/App.js
@@ -1,32 +1,45 @@
-import './styles/App.css';
-import Navbar from './components/Navbar'
-import { Route, Switch } from 'react-router';
-import WatchList from './components/WatchList';
-import Home from './components/Home';
-import StockDetails from './components/StockDetails';
-import Contact from './components/Contact';
-import AddFormWatchList from './components/auxComponents/AddFormWatchList';
-import { ContextProvider} from './components/auxComponents/StockContext';
-import Footer from './components/auxComponents/Footer';
-
+import "./styles/App.css";
+import Navbar from "./components/Navbar";
+import { Route, Switch } from "react-router";
+import WatchList from "./components/WatchList";
+import Home from "./components/Home";
+import StockDetails from "./components/StockDetails";
+import Contact from "./components/Contact";
+import AddFormWatchList from "./components/auxComponents/AddFormWatchList";
+import { ContextProvider } from "./components/auxComponents/StockContext";
+import Footer from "./components/auxComponents/Footer";
 
 function App() {
   return (
     <ContextProvider>
       <div className="App">
-      <Navbar/>
-      <Switch>
-        <Route exact path='/' render={(props) => <Home {...props}/>}/>
-        <Route exact path='/WatchList' render={(props) => <WatchList {...props}/>}/>
-        <Route exact path='/StockDetails' render={(props) => <StockDetails {...props}/>}/>
-        <Route exact path='/Contact' render={(props) => <Contact {...props}/>}/>
-        <Route exact path='/Search-WatchList' render={(props) => <AddFormWatchList {...props}/>}/>
-      </Switch>
-      <Footer/>
+        <Navbar />
+        <Switch>
+          <Route exact path="/" render={(props) => <Home {...props} />} />
+          <Route
+            exact
+            path="/WatchList"
+            render={(props) => <WatchList {...props} />}
+          />
+          <Route
+            exact
+            path="/StockDetails"
+            render={(props) => <StockDetails {...props} />}
+          />
+          <Route
+            exact
+            path="/Contact"
+            render={(props) => <Contact {...props} />}
+          />
+          <Route
+            exact
+            path="/Search-WatchList"
+            render={(props) => <AddFormWatchList {...props} />}
+          />
+        </Switch>
+        <Footer />
       </div>
-
-    </ContextProvider> 
-  
+    </ContextProvider>
   );
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import MainPicture from '../images/mainPic.jpg'
-import Footer from './auxComponents/Footer';
+import React from "react";
+import MainPicture from "../images/mainPic.jpg";
+import Footer from "./auxComponents/Footer";
 
 function Home(props) {
-    return (
-        <div className="home">
-            <div className="home-hero">
-                <img src={MainPicture} alt="chart"/>
-            </div>
-            <div className="home-content">
-                <p><b>Trade-Smart</b> is an application that tracks the changes on different companies listed on the Stock Market. Is meant to help with spotting unusual activity on the list of stock of your choosing.</p>
-                <br/>
-                <p>To achieve this it combines few APIs:
-                    <ul>One for displaying the Charts,</ul>
-                    <ul>One for displaying the news,</ul>
-                    <ul>And a Third one for Displaying the stock Details</ul>
-                </p>
-            </div>   
-        </div>
-    );
+  return (
+    <div className="home">
+      <div className="home-hero">
+        <img src={MainPicture} alt="chart" />
+      </div>
+      <section id="banner_desktop">
+        {" "}
+        This aplication is optimized for a desktop experience
+      </section>
+      <div className="home-content">
+        <p>
+          <b>Trade-Smart</b> is an application that tracks the changes on
+          different companies listed on the Stock Market. Is meant to help with
+          spotting unusual activity on the list of stock of your choosing.
+        </p>
+        <br />
+        <p>
+          To achieve this it combines few APIs:
+          <ul>One for displaying the Charts,</ul>
+          <ul>One for displaying the news,</ul>
+          <ul>And a Third one for Displaying the stock Details</ul>
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
