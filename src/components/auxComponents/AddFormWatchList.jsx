e458f94138463d24bea5931249ca8fca6762ad0f
john-v77 <tinyZ@tutanota.com>
2021-07-09 20:45:17 -0400

    try catch block on AddForm

--- src/components/auxComponents/AddFormWatchList.jsx
+++ src/components/auxComponents/AddFormWatchList.jsx
@@ -1,60 +1,71 @@
-import React, { useContext, useState } from 'react';
-import { Link } from 'react-router-dom';
-import {actions} from './APi'
-import {StocksContext} from './StockContext';
+import React, { useContext, useState } from "react";
+import { Link } from "react-router-dom";
+import { actions } from "./APi";
+import { StocksContext } from "./StockContext";
 function AddFormWatchList(props) {
-    //search the api for data on the symbol
-    // if null is return - promt the user to choose a diferent one - current is incorrect
-    // 
-    // if data is return the stock is added to the local storage
+  //search the api for data on the symbol
+  // if null is return - promt the user to choose a diferent one - current is incorrect
+  //
+  // if data is return the stock is added to the local storage
+  let message = "";
+  let [symbol, setSymbol] = useState([""]);
+  let [symbolName, SetSymbolName] = useState("");
+  const [stocksList, setStocksList] = useContext(StocksContext);
 
-    let [symbol, setSymbol] = useState([''])
-    let [symbolName, SetSymbolName] = useState('')
-    const [stocksList, setStocksList] = useContext(StocksContext);
+  localStorage.setItem("watchList", JSON.stringify(stocksList));
 
-    localStorage.setItem('watchList', JSON.stringify(stocksList))
-    
-    const recordValue = (e) =>{
-        setSymbol(e.target.value)
+  const recordValue = (e) => {
+    setSymbol(e.target.value);
+  };
+
+  const searchStock = async (e) => {
+    e.preventDefault();
+    try {
+      await actions.getStockName(symbol).then((res) => {
+        SetSymbolName(res.data.companyName);
+      });
+    } catch (error) {
+      SetSymbolName("Stock not found try another one");
     }
+    //if the ticker exists will be added to the watch list
+  };
 
-    const searchStock = (e) => {
-        e.preventDefault()
-        actions.getStockName(symbol)
-            .then(res => {
-                SetSymbolName(res.data.companyName)
-  
-            })
+  const addStockToWatchList = async (e) => {
+    e.preventDefault();
+    setStocksList((curr) => [...curr, symbol]);
+    await localStorage.setItem("watchList", JSON.stringify(stocksList));
+    message = "Stock was added to the list";
+  };
 
-        //if the ticker exists will be added to the watch list
-        if(symbolName){
-            SetSymbolName("Stock not found try another one") 
-        }
-    }
-
-    const addStockToWatchList = async (e)=>{
-        setStocksList(curr => [...curr, symbol])
-        await localStorage.setItem('watchList', JSON.stringify(stocksList))
-    }
-
-
-
-    return (
-            <div className='search-form-addStock'>
-                <form onSubmit={searchStock} className="search-for-Watch-List">
-                    <input type='text' onChange={recordValue}/>
-                    <button>search</button>
-                </form>
-                <br/><br/><br/>
-                <div className="result-add-watchList">
-                    <div>{symbolName}</div>
-                    <br/>
-                    <button onClick={addStockToWatchList}>add</button>
-                </div>
-                <br/><br/><br/>
-                <Link to='/WatchList'>go back</Link>
-            </div>
-    );
+  return (
+    <div className="search-form-addStock">
+      <p>
+        Stock can be searched and added here based on their ticker, for example:
+        tsla, appl, v
+        <br />
+        are to stock tickers for Tesla, Apple and Visa
+      </p>
+      <form onSubmit={searchStock} className="search-for-Watch-List">
+        <input type="text" onChange={recordValue} />
+        <button>search</button>
+      </form>
+      <br />
+      <br />
+      <br />
+      <div className="result-add-watchList">
+        <div>
+          {symbolName}
+          {message}
+        </div>
+        <br />
+        <button onClick={addStockToWatchList}>add</button>
+      </div>
+      <br />
+      <br />
+      <br />
+      <Link to="/WatchList">go back</Link>
+    </div>
+  );
 }
 
 export default AddFormWatchList;
