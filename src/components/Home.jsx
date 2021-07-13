e3f3d2f06e3bb2ce31a27b32554df92bc6c93962
John-v77 <ionut_vasile85@hotmail.com>
2021-07-13 14:46:36 -0400

    Home page finished

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,15 +5,19 @@ import Footer from './auxComponents/Footer';
 function Home(props) {
     return (
         <div className="home">
-            <div>
+            <div className="home-hero">
                 <img src={MainPicture} alt="chart"/>
             </div>
-            <p><b>Trade-Smart</b> is an application that tracks the changes on different companies listed on the Stock Market. Is meant to help with spotting unusual activity on the list of stock of your choosing.</p>
-            <p>It uses a few API’s:
-                <ul>One for displaying the Charts,</ul>
-                <ul>One for displaying the news,</ul>
-                <ul>And a Third one for Displaying the stock Details</ul>
-            </p>   
+            <div className="home-content">
+                <p><b>Trade-Smart</b> is an application that tracks the changes on different companies listed on the Stock Market. Is meant to help with spotting unusual activity on the list of stock of your choosing.</p>
+                <br/>
+                <p>To achieve this it combines few APIs:
+                    <ul>One for displaying the Charts,</ul>
+                    <ul>One for displaying the news,</ul>
+                    <ul>And a Third one for Displaying the stock Details</ul>
+                </p>
+            </div>   
+            <Footer/>
         </div>
     );
 }
