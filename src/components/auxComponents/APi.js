c4d9863fd398ca0245621367b582481d4dca16d9
John-v77 <ionut_vasile85@hotmail.com>
2021-07-23 21:33:43 -0400

    api update token

--- src/components/auxComponents/APi.js
+++ src/components/auxComponents/APi.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 
-const token = 'pk_695271cfa88a4f01969c642eb1576b3f';
+// const token = 'pk_695271cfa88a4f01969c642eb1576b3f';
+const token = 'pk_c7b814fba9a24e41968fa5eb41f9a1d3';
 const interval = 'intraday-prices'
 
 export const actions = {
