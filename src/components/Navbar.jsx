2333ca5ba535acf2ebfdae0d170ec27feeb04f93
john-vasile <john.Vs11@hotmail.com>
2022-10-10 14:53:34 -0400

    style change navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,10 +6,12 @@ function Navbar(props) {
   return (
     <div className='navBar shadow'>
       <img src={logo} alt='logo' />
-      <Link to='/'>Home</Link>
-      <Link to='/StockDetails'>StockDetails</Link>
-      <Link to='/WatchList'>Watch List</Link>
-      <Link to='/Contact'>Contact</Link>
+      <div className='navBarLinks'>
+        <Link to='/'>Home</Link>
+        <Link to='/StockDetails'>StockDetails</Link>
+        <Link to='/WatchList'>Watch List</Link>
+        <Link to='/Contact'>Contact</Link>
+      </div>
     </div>
   );
 }
