15fc9ee7782646473c3ca3489d8630192541ae8b
john-vasile <john.Vs11@hotmail.com>
2022-10-11 18:42:40 -0400

    fix API token

--- src/components/auxComponents/APi.js
+++ src/components/auxComponents/APi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const token = process.env.REACT_APP_TOKEN1
+const token = process.env.REACT_APP_TOKEN2
 const link = process.env.REACT_APP_LINK
 
 const interval = 'intraday-prices'
